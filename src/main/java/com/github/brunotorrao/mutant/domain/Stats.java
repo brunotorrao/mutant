412aeacff31250e7f290e96154bd051ac2425c07
Bruno Torrão <brunomattostorrao@gmail.com>
2018-10-04 11:44:43 -0300

    refactor: aggregation stats

--- src/main/java/com/github/brunotorrao/mutant/domain/Stats.java
+++ src/main/java/com/github/brunotorrao/mutant/domain/Stats.java
@@ -5,13 +5,6 @@ import lombok.Data;
 
 @Data
 public class Stats {
-    
-    public Stats(Integer countHumanDna, Integer countMutantDna) {
-        this.countHumanDna = countHumanDna;
-        this.countMutantDna = countMutantDna;
-        this.ratio = countHumanDna <= 0 ? 0 : countMutantDna.doubleValue() / countHumanDna.doubleValue();
-    }
-    
     @JsonProperty("count_mutant_dna")
     private Integer countMutantDna;
     @JsonProperty("count_human_dna")
--- src/main/java/com/github/brunotorrao/mutant/repository/MutantRepositoryImpl.java
+++ src/main/java/com/github/brunotorrao/mutant/repository/MutantRepositoryImpl.java
@@ -1,17 +1,16 @@
 package com.github.brunotorrao.mutant.repository;
 
 import com.github.brunotorrao.mutant.domain.Stats;
-import com.github.brunotorrao.mutant.domain.StatsAggregate;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
-
-import static java.lang.Boolean.FALSE;
-import static java.lang.Boolean.TRUE;
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.group;
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.newAggregation;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.project;
+import static org.springframework.data.mongodb.core.aggregation.ConditionalOperators.when;
+import static org.springframework.data.mongodb.core.query.Criteria.where;
 
 @Repository
 public class MutantRepositoryImpl implements MutantCustomRepository {
@@ -22,26 +21,26 @@ public class MutantRepositoryImpl implements MutantCustomRepository {
         this.mongoTemplate = mongoTemplate;
     }
     
-    @SuppressWarnings("unchecked")
     public Mono<Stats> getStats() {
-        var aggregation = newAggregation(group("$mutant").count().as("count"));
         
-        return mongoTemplate.aggregate(aggregation, "mutants", StatsAggregate.class)
+        var condMutant = when(where("mutant").is(true)).then(1).otherwise(0);
+        var condNonMutant = when(where("mutant").is(false)).then(1).otherwise(0);
+        
+        Aggregation aggregation = newAggregation(
+            project()
+                .and(condMutant).as("countMutantDna")
+                .and(condNonMutant).as("countHumanDna")
+                .and("1").as("aux"),
+            group("$aux")
+                .sum("countHumanDna").as("countHumanDna")
+                .sum("countMutantDna").as("countMutantDna"),
+            project("countHumanDna", "countMutantDna")
+                .and("countMutantDna").divide("countHumanDna").as("ratio")
+        );
+    
+        return mongoTemplate.aggregate(aggregation, "mutants", Stats.class)
             .collectList()
-            .map(this::buildStats);
-    }
-    
-    private Integer getCount(List<StatsAggregate> aggregates, Boolean isMutant) {
-        return aggregates
-            .stream()
-            .filter(aggregate -> aggregate.getMutant() == isMutant)
-            .findFirst()
-            .orElse(new StatsAggregate())
-            .getCount();
-    }
-    
-    private Stats buildStats(List<StatsAggregate> aggregates) {
-        return new Stats(getCount(aggregates, FALSE), getCount(aggregates, TRUE));
+            .map(x -> x.get(0));
     }
 }
 
